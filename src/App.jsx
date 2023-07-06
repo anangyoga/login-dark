b20f449509f96c8501456a28d89cc6577efa1ddb
anangyoga <aharwinto@gmail.com>
2023-07-06 14:22:26 +0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-semibold underline">Hello world!</h1>
+      <h1 className="text-3xl font-medium underline">Hello world!</h1>
+      <h1 className="text-3xl font-normal underline">Hello world!</h1>
+      <h1 className="text-3xl font-light underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
